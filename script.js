5d6132d557df873ced043062a5c4c683cd3060b5
Ellielton <ellieltongama@gmail.com>
2025-07-11 00:16:53 -0300

    Finalizado o algoritmo que recebe dois números e informa qual o maior

---
+++ script.js
@@ -0,0 +1,34 @@
+//Dia_1 - Faça um algoritmo que peça dois números e verifique (Usando if e else) e imprima o maior deles.
+
+/* 
+    inicio
+        leia: num1
+        leia: num2
+        se num1 = num2 então 
+            escreve: "Os números informados são iguais!"
+        senão se num1 > num2 então
+            escreve: "O maior número informado foi num1"
+        senão
+            escreve: "O maior número informado foi num2"
+    fim
+        
+ */
+//entrada de dados
+const num1 = parseInt(prompt("digite um número!"));
+const num2 = parseInt(prompt("digite outro número!"));
+
+//processamento
+if(num1 === num2) {
+    //saida
+    alert("Os números informados são iguais!!!");
+
+} else if (num1 > num2) {
+    //saida
+    alert("O número maior informado foi: " + num1);
+
+} else {
+    //saida
+    alert("O número maior informado foi: " + num2);
+}
+
+// .......................................................................................................10/07/2025    00:05h
