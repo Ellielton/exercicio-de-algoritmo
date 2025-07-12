9746955389e622f8c263f58f6e16b713093385d4
Ellielton <ellieltongama@gmail.com>
2025-07-11 22:46:13 -0300

    Preparando os arquivos base para o  proximo exerício

---
+++ dia3/script.js
@@ -0,0 +1,9 @@
+//Dia_3 - Faça um programa que verifique (Usando if e else) se uma letra digitada é "F" ou "M". Conforme a letra escreva: F-Feminino, M-Masculino, Sexo inválido.
+
+/* 
+    inicio
+    fim
+        
+*/
+
+// .......................................................................................................xx/07/2025    xx:xxh
